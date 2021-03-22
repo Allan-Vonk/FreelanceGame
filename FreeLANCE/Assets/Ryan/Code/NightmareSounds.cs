f4574373034df208caf518d1dfe2b0c158fbe02e
Aphaloxide <ryanblazer01@gmail.com>
2021-03-22 10:10:30 +0100

    Spooky noices go brrrr

--- FreeLANCE/Assets/Ryan/Code/NightmareSounds.cs
+++ FreeLANCE/Assets/Ryan/Code/NightmareSounds.cs
@@ -7,24 +7,33 @@ public class NightmareSounds : MonoBehaviour
     [Header("FMod Settings")]
     [SerializeField] [FMODUnity.EventRef] private string m_footstepEventPath;
     [SerializeField] [FMODUnity.EventRef] private string m_GhostSoundsEventPath;
-    [SerializeField] [FMODUnity.EventRef] private string m_GhostWindEventPath;
     [SerializeField] [FMODUnity.EventRef] private string m_JumpscareEventPath;
 
     [Header("Playback Settings")]
+    [SerializeField] private GameObject m_player;
     [SerializeField] private float m_stepDistance = 2f;
+    [SerializeField] private float m_soundTriggerDistance;
 
     private float m_stepRandom;
     private Vector3 m_prevPos;
     private float m_distanceTravelled;
     private float m_timeTakenSinceStep;
+    private float m_timeLeft;
 
     private void Start()
     {
         m_stepRandom = Random.Range(0f, .5f);
         m_prevPos = transform.position;
+        m_timeLeft = Random.Range(3f, 6f);
     }
 
     private void Update()
+    {
+        StepCheck();
+        DistanceCheck();
+    }
+
+    private void StepCheck()
     {
         m_timeTakenSinceStep += Time.deltaTime;
         m_distanceTravelled += (transform.position - m_prevPos).magnitude;
@@ -37,11 +46,33 @@ public class NightmareSounds : MonoBehaviour
         m_prevPos = transform.position;
     }
 
+    private void DistanceCheck()
+    {
+        m_timeLeft -= Time.deltaTime;
+        if (m_timeLeft <= 0)
+        {
+            if (Vector3.Distance(transform.position, m_player.transform.position) <= m_soundTriggerDistance)
+            {
+                PlaySound(m_GhostSoundsEventPath);
+            }
+
+            m_timeLeft = Random.Range(3f, 6f);
+        }
+    }
+
     private void PlaySound(string eventPath)
     {
         FMOD.Studio.EventInstance _sound = FMODUnity.RuntimeManager.CreateInstance(eventPath);
         FMODUnity.RuntimeManager.AttachInstanceToGameObject(_sound, transform, GetComponent<Rigidbody>());
         _sound.start();
         _sound.release();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "FP")
+        {
+            PlaySound(m_JumpscareEventPath);
+        }
     }
 }
--- FreeLANCE/Assets/Ryan/Code/SoundManager.cs
+++ FreeLANCE/Assets/Ryan/Code/SoundManager.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("FMod Settings")]
+    [SerializeField] [FMODUnity.EventRef] private string m_crowsEventPath;
+    [SerializeField] [FMODUnity.EventRef] private string m_crowsFlyEventPath;
+    [SerializeField] [FMODUnity.EventRef] private string m_FoliageEventPath;
+    [SerializeField] [FMODUnity.EventRef] private string m_windEventPath;
+
+    [Header("Playback Settings")]
+    [SerializeField] private GameObject m_player;
+
+    private float m_crowTimer;
+    private float m_crowFlyTimer;
+    private float m_foliageTimer;
+    private float m_windTimer;
+
+    private void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void PlaySound(string eventPath, Transform eventLocation)
     {
-        
+        FMOD.Studio.EventInstance _sound = FMODUnity.RuntimeManager.CreateInstance(eventPath);
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(_sound, eventLocation, GetComponent<Rigidbody>());
+        _sound.start();
+        _sound.release();
     }
 }
